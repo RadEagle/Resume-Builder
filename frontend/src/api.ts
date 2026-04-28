fd762544ce6e146ff825b54fe60cf5ed20c76e04
RadEagle <jonpaul2000@icloud.com>
2026-04-28 12:10:18 -0700

    refactor frontend structure and improve component organization. remove frustration on cursor

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,2 +1,13 @@
-const apiUrl = import.meta.env.VITE_API_URL // use this for Vite
-export { apiUrl };
+// use this for Vite
+const base = import.meta.env.VITE_API_URL.replace(/\/$/, '') + '/';
+
+
+// build URLs
+function buildUrl(path: string) {
+    if (base == undefined) {
+        throw new Error('API URL is not defined');
+    }
+    return new URL(path, base).toString();
+}
+
+export { buildUrl };
